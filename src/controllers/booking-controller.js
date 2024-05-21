25b566e240159cb7cd8fdf63fec3f498058f3c37
Gaurav <gaurav.bhe.24@gmail.com>
2024-05-21 15:50:02 +0530

    Added rabbit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,30 +2,50 @@ const { StatusCodes } = require('http-status-codes');
 
 const { BookingService } = require('../services/index');
 
+const { createChannel, publishMessage } = require('../utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('../config/serverConfig');
+
+
 const bookingService = new BookingService();
 
-const create = async (req, res) => {  
-    try {
-        const response = await bookingService.createBooking(req.body);
-        console.log("From BOOKING controller", response)
-        return res.status(StatusCodes.OK).json({
-            message: 'SuccessFully completed booking',
-            success: true,
-            err: {},
-            data: response
+class BookingController {
 
-        })
-    } catch (error) {
-        console.log("FROM BOOKING CONTROLLER", error);
-        return res.status(error.statusCode).json({
-            message: error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
+    constructor(){
+        
+    }
+
+    async sendMessageToQueue(req, res) {
+        const channel = await createChannel();
+        const data = {message: 'Success'}; 
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: 'Succesfully published the evnt'
         });
+    }
+
+    async create (req, res) {  
+        try {
+            const response = await bookingService.createBooking(req.body);
+            console.log("From BOOKING controller", response)
+            return res.status(StatusCodes.OK).json({
+                message: 'SuccessFully completed booking',
+                success: true,
+                err: {},
+                data: response
+    
+            })
+        } catch (error) {
+            console.log("FROM BOOKING CONTROLLER", error);
+            return res.status(error.statusCode).json({
+                message: error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            });
+        }
     }
 }
 
-module.exports = {
-    create
-}
+
+
+module.exports = BookingController
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,4 +2,5 @@
 
 module.exports = {
     BookingController: require('./booking-controller')
+    
 } 
